6a89324c44446f898b07d15f932205802de79dbf
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-12-04 00:42:35 -0800

    creating navbar footer and pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import{ Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./components"
+import { Navbar, Exchanges, Homepage, CryptoDetails, News, Cryptorcurrencies } from "./components"
+import './App.css'
 
 const App = () => {
   return (
@@ -11,12 +12,47 @@ const App = () => {
             <Navbar />
         </div>
         <div className='main'>
+            <Layout>
+                <div className='routes'>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Homepage />         
+                        </Route>
 
+                        <Route exact path='/exchanges'>
+                            <Exchanges />         
+                        </Route>
+
+                        <Route exact path='/cryptocurrencies'>
+                            <Cryptorcurrencies />         
+                        </Route>
+
+                        <Route exact path='/crypto/:coinId'>
+                            <CryptoDetails />         
+                        </Route>
+
+                        <Route exact path='/news'>
+                            <News />         
+                        </Route>
+
+                    </Switch>
+                </div>
+            </Layout>
+
+        
+            <div className='footer' >
+            <Typography.Title level={5} style={{color: 'white', textAlign: 'center' }}>
+                WiseCrypto <br />
+                All rights reserved
+            </Typography.Title>
+            <Space>
+                <Link to="/">Home</Link>
+                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/news">News</Link>
+            </Space>
+
+            </div>
         </div>
-        <div className='footer'>
-
-        </div>
-
     </div>
   )
 }
