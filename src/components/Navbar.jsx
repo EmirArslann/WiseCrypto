0535c999d3a12033de0c5ce286963fa74af216cc
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-12-06 14:42:08 -0800

    fixing css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className='nav-container'>
         <div className='logo-container'>
-            <Avatar src={icon}  size="large "/>
+            <Avatar src={icon}  size="large"/>
             <Typography.Title level={2} className="logo">
                 <Link to="/">WiseCrypto</Link>    
             </Typography.Title> 
