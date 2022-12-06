93fd7fb046d42c36f3c2656c3ef40164cb78aa72
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-12-06 14:26:01 -0800

    fetching api for homepage and Cryptocurrencies

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,16 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 const cryptoApiHeaders = {
-  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
-  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+    'X-RapidAPI-Key': '287fc765f6mshd804685e91bbf46p1581d7jsn15f4f0da9b97',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 };
+
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
-    getCryptos: builder.query({
+        getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
 
@@ -39,3 +42,5 @@ export const {
   useGetExchangesQuery,
   useGetCryptoHistoryQuery,
 } = cryptoApi;
+
+
