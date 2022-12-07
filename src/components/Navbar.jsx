07d25daced7aa6229c3d59ee41517e55d5e6c4e8
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-12-07 12:16:35 -0800

    exchanges page and charts for every cryptocurrency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React  from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link} from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../images/logo.png'
+import { useState } from 'react';
+import { useEffect } from 'react';
 const Navbar = () => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null);
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize)
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize)
+    }, []);
+
+    useEffect(() => {
+        if(screenSize< 768){
+            setActiveMenu(false)
+        }else{
+            setActiveMenu(true)
+        }
+
+    }, [screenSize])
+
+
   return (
     <div className='nav-container'>
         <div className='logo-container'>
@@ -12,6 +37,11 @@ const Navbar = () => {
             <Typography.Title level={2} className="logo">
                 <Link to="/">WiseCrypto</Link>    
             </Typography.Title> 
+            <Button className='menu-control-container' onClick={() => setActiveMenu(!activeMenu)}>
+                <MenuOutlined></MenuOutlined>
+            </Button>
+        </div>
+        {activeMenu && (
             <Menu theme='dark'>
                 <Menu.Item icon={<HomeOutlined />}>
                     <Link to="/">Home</Link>
@@ -31,9 +61,13 @@ const Navbar = () => {
 
 
             </Menu>
+        )}
 
-        </div>
+        
+            
+
     </div>
+    
   )
 }
 
