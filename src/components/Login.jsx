a5c78fdd21b1472c1818902e58b7a14b57be000c
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-12-16 02:26:37 -0800

    Finishing project

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,21 @@
+// how to hide log in form when logged in ?
+
+
+
 import React, { useState } from "react";
+
+
 
 export const Login = (props) => {
     const [Email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(Email);
+        
         try {
             const body = {Email, password};
             
@@ -23,17 +32,18 @@ export const Login = (props) => {
         }
     }
 
+   
     return (
-        <div className="auth-form-container">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
+        <div  id="form-container "className="auth-form-container">
+            <h2 className="Login-header">Login</h2>
+            <form className="formlog" onSubmit={handleSubmit}>
                 <label htmlFor="email">email</label>
                 <input value={Email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
+                <button   className="buton" type="submit">Log In</button>
             </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+            <button id="btn"className="buton" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
         </div>
     )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -27,16 +27,16 @@ export const Register = (props) => {
     return (
         <div className="auth-form-container">
             <h2>Register</h2>
-        <form className="register-form" onSubmit={handleSubmit}>
+        <form className="formlog" onSubmit={handleSubmit}>
             <label htmlFor="name">Full name</label>
             <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
             <label htmlFor="email">email</label>
             <input value={Email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">password</label>
             <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Log In</button>
+            <button className="buton" type="submit">Log In</button>
         </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+        <button className="buton" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
     )
 }
